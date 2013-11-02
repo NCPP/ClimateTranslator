1da854c625b81e12a099f583c28ca810dc63086a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-11-02 03:34:04 -0600

    Definining Datasets API.

--- ncpp/config/datasets.py
+++ ncpp/config/datasets.py
@@ -1,6 +1,7 @@
 import abc
 from ncpp.util.data_scanner.query import DataQuery
 from ncpp.constants import DATASETS_DB, NO_VALUE_OPTION
+from abc import abstractmethod
 
 VARIABLE = 'variable'
 PACKAGE = 'package'
@@ -15,6 +16,36 @@ class Datasets(object):
     
     def __init__(self):
         pass
+
+    
+    @abstractmethod
+    def getDataTypes(self):
+        '''
+        Method to return the possible choices of selecting datasets.
+        '''
+        pass
+    
+    @abstractmethod
+    def getDatasetOptions(self, data_type, long_name=None, time_frequency=None, dataset_category=None, dataset=None,
+                                dataset_category2=None, package_name=None):
+        '''
+        Method to return the available options for the dataset selection fields.
+        Returns a dictionary of (field_name, field_values{}) pairs. 
+        Example:
+        {'long_name': [u'Air Temperature', u'Daily Precipitation Rate', u'Maximum Air Temperature', u'Minimum Air Temperature'], 'time_frequency': [u'day'], 
+         'dataset_category': [u'Downscaled', u'Gridded Observational'], 'dataset': [u'Hayhoe ARRM-GFDL', u'Maurer 2010']}
+        '''
+        
+        pass
+    
+    @abstractmethod
+    def getDatasets(self, data_type, long_name=None, time_frequency=None, 
+                         dataset_category=None, dataset=None, package_name=None):
+        
+        '''Method to return the datasets for a given selection.
+           Returns a list of dictionaries, one for each dataset.'''
+
+        pass
     
 class DbDatasets(Datasets):
     '''Implementation of Datasets API that retrieves information from a database back-end.'''
@@ -23,9 +54,6 @@ class DbDatasets(Datasets):
         self.dataQuery = DataQuery(db_path=db_path)
         
     def getDataTypes(self):
-        '''
-        Method to return the possible choices of selecting datasets.
-        '''
 
         tuples = []
         tuples.append( (VARIABLE, VARIABLE.capitalize()) ) # (key, label)
@@ -34,14 +62,7 @@ class DbDatasets(Datasets):
         return tuples
 
     def getDatasetOptions(self, data_type, long_name=None, time_frequency=None, dataset_category=None, dataset=None,
-                                     dataset_category2=None, package_name=None):
-        '''
-        Method to return the available options for the dataset selection fields.
-        Returns a dictionary of (field_name, field_values{}) pairs. 
-        Example:
-        {'long_name': [u'Air Temperature', u'Daily Precipitation Rate', u'Maximum Air Temperature', u'Minimum Air Temperature'], 'time_frequency': [u'day'], 
-         'dataset_category': [u'Downscaled', u'Gridded Observational'], 'dataset': [u'Hayhoe ARRM-GFDL', u'Maurer 2010']}
-        '''
+                                dataset_category2=None, package_name=None):
         
         if data_type=='variable':
             dict = self.dataQuery.get_variable_or_index('variable', long_name=long_name, time_frequency=time_frequency, 
