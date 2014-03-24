0cfff3a90142afdb31cfef7b32093a98109198f8
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-03-24 13:38:30 -0600

    Last changes based on NCPP team feedback.

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Denver'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -73,7 +73,7 @@ TEMPLATE_LOADERS = (
 # makes 'request' object available in templates
 TEMPLATE_CONTEXT_PROCESSORS += (
      'django.core.context_processors.request',
-) 
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
